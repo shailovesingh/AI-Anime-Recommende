e1af7cabd731959df999b6ee3474a34c254ea00a
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-01-17 12:33:26 +0530

    git commit -m "pipeline"

---
+++ pipeline/build_pipeline.py
@@ -0,0 +1,14 @@
+from src.data_loader import AnimeDataLoader
+from src.vector_store import VectorStoreBuilder
+from dotenv import load_dotenv
+from utils.logger import get_logger
+from utils.custom_exception import CustomException
+
+load_dotenv()
+
+logger = get_logger()
+
+def main():
+    try:
+        logger.info("Starting to build pipeline...")
+        
