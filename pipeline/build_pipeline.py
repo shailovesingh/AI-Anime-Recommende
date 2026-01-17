f9809f813e83417c44f8279a5ba741168b7ab1d4
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-01-17 12:53:59 +0530

    git commit -m "build_pipeline"

--- pipeline/build_pipeline.py
+++ pipeline/build_pipeline.py
@@ -6,9 +6,29 @@ from utils.custom_exception import CustomException
 
 load_dotenv()
 
-logger = get_logger()
+logger = get_logger(__name__)
 
 def main():
     try:
         logger.info("Starting to build pipeline...")
+
+        loader = AnimeDataLoader("data/anime_with_synopsis.csv", "data/anime_updated.csv")
+
+        processed_csv = loader.load_and_process()
+
+        logger.info("Data loaded and processed...")
         
+        vector_builder = VectorStoreBuilder(processed_csv)
+        vector_builder.build_and_save_vectorstore()
+
+        logger.info("Vector store Built succesfully....")
+
+        logger.info("Pipeline built successfuly..")
+
+    except Exception as e:
+        logger.error(f"Failed to execute pipeline {str(e)}")
+        raise CustomException("Error during pipline ", e)
+
+
+if __name__ == "__main__":
+    main()
--- src/data_loader.py
+++ src/data_loader.py
@@ -6,7 +6,7 @@ class AnimeDataLoader:
         self.processed_csv = processed_csv
 
     def load_and_process(self):
-        df = pd.read_csv(self.original_csv, encoding='utf-8', error_bad_lines=False).dropna()
+        df = pd.read_csv(self.original_csv, encoding='utf-8', on_bad_lines='skip').dropna()
         required_cols = {'Name', 'Genres', 'synopsis'}
 
         missing = required_cols - set(df.columns)
